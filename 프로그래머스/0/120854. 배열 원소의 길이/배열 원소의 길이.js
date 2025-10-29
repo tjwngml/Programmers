0da5932e209ef35cdcfb3e9c9a64f62b5c83ad4d
tjwngml <tjwngml0306@gmail.com>
2025-10-29 23:38:04 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,11 @@
+function solution(strlist) {
+        
+    let resultArray = []
+    for(i=0;i<strlist.length;i++){
+      let eachWord = strlist[i];
+      let answer = eachWord.length;
+       resultArray.push(answer);
+        
+     }return resultArray
+
+}
